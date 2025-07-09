76d3f08b19dfd3a3863937ccbf5387d084a257c2
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-07-09 21:38:27 +0530

    Updated the solution for other problem

--- 53. Maximum Subarray/src/Solution.java
+++ 53. Maximum Subarray/src/Solution.java
@@ -1,22 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-
         int currentSum = 0;
         int maxSum = Integer.MIN_VALUE;
-        int counter = 0;
-        int size = nums.length;
-        int i = 0;
-
-        while (i < size) {
+        for (int i = 0; i < nums.length; i++) {
             currentSum += nums[i];
             maxSum = Math.max(maxSum, currentSum);
-            if (currentSum < maxSum) {
-                while (currentSum < maxSum && counter < size) {
-                    currentSum = currentSum - nums[counter];
-                    counter++;
-                }
+            if (currentSum < 0) {
+                currentSum = 0;
             }
-            i++;
         }
         return maxSum;
     }
