9f6d98ecaf9d228559255d963e7bd9c5734419ee
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2025-07-11 21:17:50 +0530

    Updated the solution for 74. Search a 2D Matrix

--- Code Code Testcase Test Result Test Result 74. Search a 2D Matrix/src/Solution.java
+++ Code Code Testcase Test Result Test Result 74. Search a 2D Matrix/src/Solution.java
@@ -2,15 +2,16 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int size = matrix[0].length;
-        for (int[] ints : matrix) {
-            if (target >= ints[0] && target <= ints[size - 1]) {
+        for (int i = 0; i < matrix.length; i++) {
+            if (target >= matrix[i][0] && target <= matrix[i][size - 1]) {
+                int[] oneDArr = matrix[i];
                 int left = 0;
                 int right = size - 1;
-                while (left < right) {
+                while (left <= right) {
                     int mid = left + (right - left) / 2;
-                    if (target == ints[mid]) {
+                    if (target == matrix[i][mid]) {
                         return true;
-                    } else if (ints[mid] < target) {
+                    } else if (oneDArr[mid] < target) {
                         left++;
                     } else {
                         right--;
